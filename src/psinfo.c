6062df2eddd951fcc25e782ac2fa52cda1ed28cb
Michael Peters <mpeters@embeddedTS.com>
2023-09-29 23:15:54 +0000

    Begin Stage 2: Process Status File

--- src/psinfo.c
+++ src/psinfo.c
@@ -12,12 +12,16 @@
 int main() {
 	pid_t pid, self;
 	char cmdlinePath[256];
+	char statusPath[256];
 	char line[256];
 
 	DIR *procDir;
 	struct dirent *procEntry;
 	FILE *cmdlineFile = NULL;
+	FILE *statusFile = NULL;
 
+	int done = 0;
+	
 	self = getpid();
 
 	procDir = opendir("/proc");
@@ -27,21 +31,42 @@ int main() {
 	}
 	int i = 0;
 
-	while ((procEntry = readdir(procDir))) {
+	while (procEntry = readdir(procDir)) {
+		done = 1;
 		pid_t pid = strtol(procEntry->d_name, NULL, 10); // XXX will this even work?
 		pid_t ppid;
 
-		printf("Process %s:  ", procEntry->d_name);
-		if(pid > 0 && !strstr(procEntry->d_name, ".")){
+		if(pid > 0){
+			printf("Process %s:  ", procEntry->d_name);
 			snprintf(cmdlinePath, sizeof(cmdlinePath), "/proc/%d/cmdline", pid);
 			cmdlineFile = fopen(cmdlinePath, "r");
+
 			if(!cmdlineFile)
 				printf("No cmdline?");
-			else
-				printf("%s/n",fgets(line, sizeof(line), cmdlineFile));
-			fclose(cmdlineFile);
-		}
+			else {
+				printf("%s ",fgets(line, sizeof(line), cmdlineFile));
+				fclose(cmdlineFile);
+			}
+
+			snprintf(statusPath, sizeof(statusPath), "/proc/%d/status", pid);
+			statusFile = fopen(statusPath, "r");
+
+			if(!statusFile)
+				printf("No status file?");
+			else {
+				while(fgets(line, sizeof(line), statusFile)) {
+					if(strstr(line, "State: ")){
+						printf("%s", line);
+						break;
+					}
+				}
+				fclose(statusFile);
+			}
+
+			if(pid == self)
+				printf(" <-- Hey that's me!");
 		printf("\n");	
+		}
 	}
 	closedir(procDir);
 	return 0;
